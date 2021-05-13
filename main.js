5006ec28620f4bcaad1c94aa632d26f33711fe91
ianmanderson92 <ianmanderson92@users.noreply.github.com>
2021-05-13 10:04:23 -0600

    Update main.js

--- main.js
+++ main.js
@@ -101,7 +101,6 @@ router.use((req, res, next) => {
   next();
 });
 
-//!!FIXME!! exoressValidator isnt a function error
 router.use(expressValidator());
 
 router.get("/", homeController.showSignIn);
@@ -113,7 +112,6 @@ router.post("/signup", usersController.validate, usersController.create, usersCo
 router.get("/logout", usersController.logout, usersController.redirectView);
 
 router.get("/home/:id", usersController.showHome, usersController.showViewHome);
-//router.get("/home/", usersController.showHome, usersController.showViewHome);
 
 router.get("/users/:id/page", usersController.showUserPage, usersController.showViewUserPage);
 
@@ -123,7 +121,6 @@ router.post("/users/login", passport.authenticate('local' , {failureRedirect:'/'
 function(req, res) {
    res.redirect(`/home/${req.user._id}`);
 });
-//router.post("/users/login", usersController.authenticate);
 
 router.get("/users/:id/posts", usersController.showPosts, usersController.showViewPosts);
 
